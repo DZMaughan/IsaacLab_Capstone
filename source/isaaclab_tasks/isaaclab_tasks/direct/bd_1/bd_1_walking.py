c27d3997421fd0676e6f2c03c167867c631e93ae
Zac <zac.maughan@usu.edu>
2026-02-27 17:48:30 -0700

    Started to build up the BD-1 Robot Folder for training

---
+++ source/isaaclab_tasks/isaaclab_tasks/direct/bd_1/bd_1_walking.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import torch
+import isaaclab.sim as sim_utils
+from isaaclab.assets import Articulation, ArticulationCfg, RigidObject, RigidObjectCfg
+from isaaclab.envs import DirectMARLEnv, DirectMARLEnvCfg
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.sim import SimulationCfg
+from isaaclab.sim.spawners.from_files import GroundPlaneCfg, spawn_ground_plane
+from isaaclab.utils import configclass
+from isaaclab_assets.robots.leatherback import LEATHERBACK_CFG  # isort: skip
+from isaaclab.markers import VisualizationMarkers, VisualizationMarkersCfg
+from isaaclab.sensors import Camera, CameraCfg, TiledCamera, TiledCameraCfg
+from isaaclab.utils.math import quat_from_euler_xyz
+import matplotlib.pyplot as plt
+
+def get_quaternion_tuple_from_xyz(x, y, z):
+    quat_tensor = quat_from_euler_xyz(torch.tensor([x]), torch.tensor([y]), torch.tensor([z])).flatten()
+    return (quat_tensor[0].item(), quat_tensor[1].item(), quat_tensor[2].item(), quat_tensor[3].item())
+
+@configclass
